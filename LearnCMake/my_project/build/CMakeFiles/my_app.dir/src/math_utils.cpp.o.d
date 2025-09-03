de8112874d44b80e4bbd3c6e15b32606b674dd9b
Bhavin <bhavinmoriya58@gmail.com>
2025-09-03 11:38:50 +0200

    FirstProject

---
+++ LearnCMake/my_project/build/CMakeFiles/my_app.dir/src/math_utils.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/my_app.dir/src/math_utils.cpp.o: \
+ /mnt/c/Users/moriyab/Downloads/learnBasics/LearnCMake/my_project/src/math_utils.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/c/Users/moriyab/Downloads/learnBasics/LearnCMake/my_project/src/math_utils.h
