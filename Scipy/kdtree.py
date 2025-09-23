c96cf22eaa26cacf1c407370172f859aa7f740ee
Bhavin <bhavinmoriya58@gmail.com>
2025-09-23 17:36:15 +0200

    .

--- Scipy/kdtree.py
+++ Scipy/kdtree.py
@@ -41,3 +41,26 @@ query_point = np.array([0.5, 0.5])
 distance, index = tree.query(query_point)
 
 print(f"Nearest neighbor: {points[index]}, Distance: {distance}")
+
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+# Plot the random points
+plt.figure(figsize=(8, 6))
+plt.scatter(points[:, 0], points[:, 1], c='blue', label='Random Points')
+
+# Plot the query point
+plt.scatter(query_point[0], query_point[1], c='red', marker='x', s=100, label='Query Point')
+
+# Plot the query point
+plt.scatter(points[index][0], points[index][1], c='magenta', marker='x', s=100, label='Nearest Point')
+
+# Add labels and legend
+plt.title('Random 2D Points with Query Point')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.legend()
+plt.grid(True)
+# Save the plot as an image file
+plt.savefig('random_points_with_query.png', dpi=300, bbox_inches='tight')
+# plt.show()
+plt.close()
